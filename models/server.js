f02829d5ba24e58367f46e39a4a8699f93a1ba8e
Jose <jcv1923cv@gmail.com>
2023-05-02 15:07:41 -0500

    ya?

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ const Sockets = require('./sockets');
 
 class Server {
     constructor(){
-        this.app = express();;
+        this.app = express();
         this.port = process.env.PORT;
 
         //http server
